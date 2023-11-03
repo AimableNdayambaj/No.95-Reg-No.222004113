4fa50b99da6f744679c68dc39d52e23aab4791e3
AimableNdayambaj <149766613+AimableNdayambaj@users.noreply.github.com>
2023-11-03 15:49:09 +0200

    Update question1.java

--- question1.java
+++ question1.java
@@ -1,12 +1,17 @@
-class question1 {
+//java program for calculating simple interest
+//define a class name 'question1'
+class question1 {//first class
+    //method to calculate simple interest
     public static double money(double p, double r, double t){
-        return p*r*t;
+        return p*r*t;//calculate and return simple interest
     }
 }
-        public class object1{
+        public class object1{//second class
             public static void main(String args[]){
+                //create an instance 'question1' class
                 question1 myquestion1= new question1();
+                //call 'money' method with specific values and store
                 double result=myobjectex.money(100000,0.05,5);
-                System.out.println("simple interest is:"+ result);
+                System.out.println("simple interest is:"+ result);//print the result
             }
         }
