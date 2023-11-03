161d649c6e369608f6c2879cd080c2c9b71fabfe
AimableNdayambaj <149766613+AimableNdayambaj@users.noreply.github.com>
2023-11-03 16:00:23 +0200

    Update questionb.java

--- questionb.java
+++ questionb.java
@@ -1,12 +1,17 @@
-class object{
+//java program for calculating compound interest
+//define a class name'object'
+class object{//first class
+     //method to calculate compound interest
     public static double money(double p,double r,double t){
-        return Math.pow(1+r,t)*p;
+        return Math.pow(1+r,t)*p;//calculate and return compound interest
     }
 }
-public class questionb{
+public class questionb{//second class
     public static void main(String args[]){
+        //create an instance 'questionb' class
         object myobject= new object();
+        //call 'money' method with specific values and store 
         double result= myobject.money(500000,0.18,3);
-        System.out.println(+ result);
+        System.out.println(+ result);//print result as compound interest
     }
 }
