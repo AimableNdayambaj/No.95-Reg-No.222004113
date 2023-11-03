c83694435ad530cddd29fda54ed62d476664ea6e
AimableNdayambaj <149766613+AimableNdayambaj@users.noreply.github.com>
2023-11-03 10:23:18 +0200

    Add files via upload
    Simple interest 
    Compound interest

---
+++ question1.java
@@ -0,0 +1,12 @@
+class question1 {
+    public static double money(double p, double r, double t){
+        return p*r*t;
+    }
+}
+        public class object1{
+            public static void main(String args[]){
+                question1 myquestion1= new question1();
+                double result=myobjectex.money(100000,0.05,5);
+                System.out.println("simple interest is:"+ result);
+            }
+        }
